4e34ee4eb3a245f1c0746ad9f6f7e9c04182a7cd
mattroz <matveyrozanov@gmail.com>
2019-11-28 17:59:27 +0300

    Implement S2-module

--- blocks.py
+++ blocks.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 class S2Block(nn.Module):
-
     def __init__(self, n_channels, feature_map_size, kernel_size, padding='same'):
         super().__init__()
 
@@ -25,5 +24,36 @@ class S2Block(nn.Module):
         x = self.pwconv(x)
         x = self.bn2(x)
         x = nn.functional.interpolate(x, size=input.shape[-2:], mode='bilinear')
+
+        return x
+
+
+class S2Module(nn.Module):
+
+    def __init__(self, n_channels, groups=1):
+        super().__init__()
+
+        self.n_channels = n_channels
+        out_channels = (n_channels // 2) if (n_channels // 2) != 0 else 1
+        self.pwconv = nn.Conv2d(n_channels, out_channels, kernel_size=1, bias=False, groups=groups)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = nn.ReLU()
+
+        '''TODO: make feature_map_size and kernel_size for S2Block as S2Module arguments'''
+        self.s2_block_1 = S2Block(out_channels, 4, 3)
+        self.s2_block_2 = S2Block(out_channels, 4, 3)
+        self.out_act = nn.PReLU()
+
+    def forward(self, input):
+        permuted_channel_idxs = torch.randperm(self.n_channels)
+        x = input[:, permuted_channel_idxs, :, :]
+        x = self.pwconv(x)
+        x = self.bn(x)
+        x = self.act(x)
+        x1 = self.s2_block_1(x)
+        x2 = self.s2_block_2(x)
+        x = torch.cat([x1, x2], dim=1)
+        x = torch.add(x, input)
+        x = self.out_act(x)
 
         return x
