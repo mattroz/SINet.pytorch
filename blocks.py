7f74193da398ca759724f4db2f39db14013f58d3
mattroz <matveyrozanov@gmail.com>
2019-12-02 13:49:36 +0300

    Add decoder with information blocking

--- blocks.py
+++ blocks.py
@@ -49,18 +49,18 @@ class S2Block(nn.Module):
 
 class S2Module(nn.Module):
 
-    def __init__(self, n_channels, block_1_args, block_2_args, groups=1):
+    def __init__(self, n_channels, out_channels, block_1_args, block_2_args, groups=1):
         super().__init__()
 
         self.n_channels = n_channels
-        out_channels = (n_channels // 2) if (n_channels // 2) != 0 else 1
-        self.pwconv = nn.Conv2d(n_channels, out_channels, kernel_size=1, bias=False, groups=groups)
-        self.bn = nn.BatchNorm2d(out_channels)
+        bneck_out_channels = (n_channels // 2) if (n_channels // 2) != 0 else 1
+        self.pwconv = nn.Conv2d(n_channels, bneck_out_channels, kernel_size=1, bias=False, groups=groups)
+        self.bn = nn.BatchNorm2d(bneck_out_channels)
         self.act = nn.ReLU()
 
-        '''TODO: make feature_map_size and kernel_size for S2Block as S2Module arguments'''
-        self.s2_block_1 = S2Block(out_channels, **block_1_args)
-        self.s2_block_2 = S2Block(out_channels, **block_2_args)
+        self.s2_block_1 = S2Block(bneck_out_channels, **block_1_args)
+        self.s2_block_2 = S2Block(bneck_out_channels, **block_2_args)
+        self.conv = nn.Conv2d(n_channels, out_channels, kernel_size=1)
         self.out_act = nn.PReLU()
 
     def forward(self, input):
@@ -78,6 +78,7 @@ class S2Module(nn.Module):
 
         x = torch.cat([x1, x2], dim=1)
         x = torch.add(x, input)
+        x = self.conv(x)
         x = self.out_act(x)
 
         return x
@@ -89,25 +90,26 @@ class SINetEncoder(nn.Module):
 
         '''Configs from paper, Table 2'''
         s2_modules_config = [
-            [16, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 5, 'avg_pool_kernel': 1}],
-            [48, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 3, 'avg_pool_kernel': 1}],
-            [48, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 5, 'avg_pool_kernel': 1}],
-            [96, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 3, 'avg_pool_kernel': 1}],
-            [96, {'kernel_size': 5, 'avg_pool_kernel': 1}, {'kernel_size': 3, 'avg_pool_kernel': 2}],
-            [96, {'kernel_size': 5, 'avg_pool_kernel': 2}, {'kernel_size': 3, 'avg_pool_kernel': 4}],
-            [96, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 3, 'avg_pool_kernel': 1}],
-            [96, {'kernel_size': 5, 'avg_pool_kernel': 1}, {'kernel_size': 5, 'avg_pool_kernel': 1}],
-            [96, {'kernel_size': 3, 'avg_pool_kernel': 2}, {'kernel_size': 3, 'avg_pool_kernel': 4}],
-            [96, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 5, 'avg_pool_kernel': 2}],
+            [16, 48, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 5, 'avg_pool_kernel': 1}],
+            [48, 48, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 3, 'avg_pool_kernel': 1}],
+            [48, 96, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 5, 'avg_pool_kernel': 1}],
+            [96, 96, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 3, 'avg_pool_kernel': 1}],
+            [96, 96, {'kernel_size': 5, 'avg_pool_kernel': 1}, {'kernel_size': 3, 'avg_pool_kernel': 2}],
+            [96, 96, {'kernel_size': 5, 'avg_pool_kernel': 2}, {'kernel_size': 3, 'avg_pool_kernel': 4}],
+            [96, 96, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 3, 'avg_pool_kernel': 1}],
+            [96, 96, {'kernel_size': 5, 'avg_pool_kernel': 1}, {'kernel_size': 5, 'avg_pool_kernel': 1}],
+            [96, 96, {'kernel_size': 3, 'avg_pool_kernel': 2}, {'kernel_size': 3, 'avg_pool_kernel': 4}],
+            [96, 96, {'kernel_size': 3, 'avg_pool_kernel': 1}, {'kernel_size': 5, 'avg_pool_kernel': 2}],
         ]
 
         self.s2_modules = []
         for i in range(len(s2_modules_config)):
             module_name = f's2_module_{i}'
             _in_channels = s2_modules_config[i][0]
-            s2_block_1_args = s2_modules_config[i][1]
-            s2_block_2_args = s2_modules_config[i][2]
-            setattr(self, module_name, S2Module(_in_channels, s2_block_1_args, s2_block_2_args))
+            _out_channels = s2_modules_config[i][1]
+            s2_block_1_args = s2_modules_config[i][2]
+            s2_block_2_args = s2_modules_config[i][3]
+            setattr(self, module_name, S2Module(_in_channels, _out_channels, s2_block_1_args, s2_block_2_args))
             self.s2_modules.append(getattr(self, module_name))
 
         self.conv2d = nn.Conv2d(in_channels, 12, kernel_size=2, stride=2)
@@ -125,9 +127,7 @@ class SINetEncoder(nn.Module):
 
         res = x
         for i in range(2):
-            print('RES: ', res.shape)
             res = self.s2_modules[i](res)
-            print('RES: ', res.shape)
 
         x = torch.cat([x, res], dim=1)
         print(x.shape)
@@ -144,6 +144,46 @@ class SINetEncoder(nn.Module):
 
         return x
 
+
+
+class SINetDecoder(nn.Module):
+
+    def __init__(self, in_channels, inf_block_channels):
+        super().__init__()
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.inf_block_conv = nn.Conv2d(inf_block_channels, in_channels, kernel_size=3, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, bias=False)
+        self.conv3 = nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.bn2 = nn.BatchNorm2d(in_channels)
+        self.inf_block_bn = nn.BatchNorm2d(in_channels)
+        self.act = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input):
+        tensor, encoder_feature_map = input
+        x = self.upsample(tensor)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.act(x)
+
+        inf_block_map = torch.max(self.softmax(x), dim=1).values
+        inf_block_branch = self.inf_block_conv(encoder_feature_map)
+        inf_block_branch = self.inf_block_bn(inf_block_branch)
+        inf_block_branch = torch.mul(inf_block_branch, inf_block_map)
+
+        x = torch.add(x, inf_block_branch)
+        x = self.upsample(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.act(x)
+
+        x = self.upsample(x)
+        x = self.conv3(x)
+
+        return x
 
 
 
